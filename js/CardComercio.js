d742db8aa1add9d688acd0d2ac7d4ab6f0178c93
Pedro Luciano <pedroluciano@Mac.lan>
2025-05-23 18:25:06 -0400

    Descripción breve del cambio

--- js/CardComercio.js
+++ js/CardComercio.js
@@ -12,12 +12,12 @@ export function cardComercio(comercio) {
 
     <div class="relative w-full flex flex-col items-center pt-7 ">
       <img src="${comercio.logo}" alt="Logo de ${comercio.nombre}"
-        class="w-20 h-20 rounded-full absolute left-1/2 -top-10 transform -translate-x-1/2 
-               bg-white object-contain shadow-[0px_-22px_38px_-22px_rgba(0,_0,_0,_0.5)] border-4 border-white z-10" />
+        class="w-24 h-24 rounded-full absolute left-1/2 -top-10 transform -translate-x-1/2 
+               bg-white object-contain shadow-[0px_-22px_38px_-22px_rgba(0,_0,_0,_0.5)] border-4 border-white z-100" />
 
       <div class="relative h-12 w-full">
         <div class="absolute inset-0 flex items-center justify-center px-2 text-center">
-          <h3 class="text-base font-bold text-gray-900 leading-tight line-clamp-2">
+          <h3 class="font-medium text-xl text-[#424242] leading-tight line-clamp-2 mt-5">
             ${comercio.nombre}
           </h3>
         </div>
@@ -25,20 +25,20 @@ export function cardComercio(comercio) {
     </div>
 
     <a href="tel:${comercio.telefono}" 
-   class="inline-flex items-center justify-center gap-2 text-white font-semibold bg-red-600 
-          rounded-full px-2 py-1.5 mb-2 shadow hover:bg-red-700 transition text-sm mx-auto">
-        <i class="fa-solid fa-phone"></i> ${comercio.telefono}
+   class="inline-flex items-center justify-center gap-2 text-lg text-white font-normal bg-red-600 
+          rounded-full px-2 py-1 mb-2 shadow hover:bg-red-700 transition mx-auto mt-3">
+        <i class="fa-solid fa-phone text-lg font-normal "></i> ${comercio.telefono}
       </a>
 
-    <div class="flex justify-center items-center gap-1 ${comercio.abierto ? 'text-green-600' : 'text-red-600'} font-medium mb-1 text-sm">
-      <i class="fas fa-clock"></i> ${comercio.abierto ? 'Abierto Ahora' : 'Cerrado Ahora'}
+    <div class="flex justify-center items-center gap-1 ${comercio.abierto ? 'text-green-600' : 'text-red-600'} font-medium mb-1 text-base">
+      <i class="far fa-clock"></i> ${comercio.abierto ? 'Abierto Ahora' : 'Cerrado Ahora'}
     </div>
 
-    <div class="flex justify-center items-center gap-1 text-blue-500 font-medium mb-1 text-sm">
-      <i class="fas fa-map-marker-alt"></i> ${comercio.pueblo}
+    <div class="flex justify-center items-center gap-1 font-medium mb-1 text-sm text-[#23b4e9]">
+      <i class="fas fa-map-pin text-[#23b4e9]"></i> ${comercio.pueblo}
     </div>
 
-    <div class="flex justify-center items-center gap-1 text-gray-500 font-medium text-sm mb-4">
+    <div class="flex justify-center items-center gap-1 text-[#9c9c9c] font-medium text-sm mb-4">
       <i class="fas fa-car"></i> ${comercio.tiempoVehiculo}
     </div>
 
