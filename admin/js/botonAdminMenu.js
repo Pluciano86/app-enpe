b684331901a432eda11af1ed69af8c7e228c311b
Pluciano86 <peter.luciano@live.com>
2026-01-23 20:39:45 -0400

    Actualización general

--- admin/js/botonAdminMenu.js
+++ admin/js/botonAdminMenu.js
@@ -1,17 +1,15 @@
 // admin/js/botonAdminMenu.js
+const COMERCIO_ORIGIN = 'https://comercio.enpe-erre.com';
 const paramsAdminMenu = new URLSearchParams(window.location.search);
-const idComercio =
-  paramsAdminMenu.get('idcomercio') || paramsAdminMenu.get('id');
+const idComercio = paramsAdminMenu.get('idcomercio') || paramsAdminMenu.get('id');
 const btnAdminMenu = document.getElementById('btnAdminMenu');
 
 if (btnAdminMenu && idComercio) {
-  const destino = new URL('/comercio/adminMenuComercio.html', window.location.origin);
+  const destino = new URL('/adminMenuComercio.html', COMERCIO_ORIGIN);
   destino.searchParams.set('idcomercio', idComercio);
-  destino.searchParams.set('id', idComercio); // compatibilidad con páginas que aún leen "id"
-  btnAdminMenu.href = destino.toString();
   console.log('REDIRECT FINAL (Menú):', destino.toString());
   btnAdminMenu.addEventListener('click', (event) => {
     event.preventDefault();
-    window.location.assign(destino.toString());
+    window.location.href = destino.toString();
   });
 }
--- admin/js/botonEspeciales.js
+++ admin/js/botonEspeciales.js
@@ -1,17 +1,15 @@
 // admin/js/botonEspeciales.js
+const COMERCIO_ORIGIN = 'https://comercio.enpe-erre.com';
 const paramsEspeciales = new URLSearchParams(window.location.search);
-const idComercioEspecial =
-  paramsEspeciales.get('idcomercio') || paramsEspeciales.get('id');
+const idComercioEspecial = paramsEspeciales.get('idcomercio') || paramsEspeciales.get('id');
 const btnAdministrarEspeciales = document.getElementById('btnAdministrarEspeciales');
 
 if (btnAdministrarEspeciales && idComercioEspecial) {
-  const destino = new URL('/comercio/especiales/adminEspeciales.html', window.location.origin);
+  const destino = new URL('/especiales/adminEspeciales.html', COMERCIO_ORIGIN);
   destino.searchParams.set('idcomercio', idComercioEspecial);
-  destino.searchParams.set('id', idComercioEspecial); // compatibilidad
-  btnAdministrarEspeciales.href = destino.toString();
   console.log('REDIRECT FINAL (Especiales):', destino.toString());
   btnAdministrarEspeciales.addEventListener('click', (event) => {
     event.preventDefault();
-    window.location.assign(destino.toString());
+    window.location.href = destino.toString();
   });
 }
